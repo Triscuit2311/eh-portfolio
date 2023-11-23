5bbfeafcf3be82af5d551aea6813632047edc18d
Dante Trisciuzzi <dante.trisciuzzi@snhu.edu>
2023-11-23 14:45:15 -0500

    gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,27 +1,34 @@
 import React, { useState, useEffect, useMemo } from 'react';
+import { useSprings, animated } from 'react-spring';
+import { useGesture } from 'react-use-gesture';
 import "@/styles/Gallery.css";
 
-export default function Gallery({ artwork_sets }) {
-
-
-
-    const minSize = 100;
+export default function FloatyGallery({ artwork_sets }) {
+    const minSize = 200;
     const maxSize = 400;
+    const ratio = 9 / 16;
     const minSlideTime = 3000;
     const maxSlideTime = 9000;
+    const marginPercent = 5;
+    const centerMarginWidthPercent = 30;
+    const centerMarginHeightPercent = 10;
+    const fake_width = 3840;
+    const fake_height = 2160;
 
-    // Assigning sizes to each art set using useMemo
-    const artSetSizes = useMemo(() => artwork_sets.map(() => ({
-        width: Math.random() * (maxSize - minSize) + minSize,
-        height: Math.random() * (maxSize - minSize) + minSize
-    })), [artwork_sets]);
+    const slideshow_mass = 20;
+    const slideshow_tension = 170;
+    const slideshow_friction = 70;
 
 
+    const artSetSizes = useMemo(() => artwork_sets.map(() => {
+        const width = Math.random() * (maxSize - minSize) + minSize;
+        const height = width * ratio;
+        return { width, height };
+    }), [artwork_sets]);
 
     const [currentSlides, setCurrentSlides] = useState(
         new Array(artwork_sets.length).fill(0)
     );
-    const [expandedSlideshow, setExpandedSlideshow] = useState(null);
 
     useEffect(() => {
         const intervals = artwork_sets.map((_, index) => {
@@ -40,41 +47,143 @@ export default function Gallery({ artwork_sets }) {
         return () => intervals.forEach(interval => clearInterval(interval));
     }, [artwork_sets]);
 
-    // Function to determine if a slide is the next slide
+
+    const getRandomPosition = (size, margin, centerMarginSize, containerSize) => {
+        let position;
+        do {
+            position = Math.random() * (containerSize - size - 2 * margin) + margin;
+        } while (position + size > (containerSize - centerMarginSize) / 2 && position < (containerSize + centerMarginSize) / 2);
+        return position;
+    };
+
+    // Server side initialization
+    const [springs, set] = useSprings(artwork_sets.length, index => {
+        const centerMarginWidth = fake_width * centerMarginWidthPercent / 100;
+        const centerMarginHeight = fake_height * centerMarginHeightPercent / 100;
+        const horizontalMargin = fake_width * marginPercent / 100;
+        const verticalMargin = fake_height * marginPercent / 100;
+        const x = getRandomPosition(artSetSizes[index].width, horizontalMargin, centerMarginWidth, fake_width);
+        const y = getRandomPosition(artSetSizes[index].height, verticalMargin, centerMarginHeight, fake_height);
+        return {
+            xy: [x, y],
+            config: { mass: slideshow_mass, tension: slideshow_tension, friction: slideshow_friction }
+        };
+    });
+
+    // Client side initialization
+    useEffect(() => {
+        set(index => {
+            const containerRect = document.querySelector('.gallery-container').getBoundingClientRect();
+
+            const centerMarginWidth = containerRect.width * centerMarginWidthPercent / 100;
+            const centerMarginHeight = containerRect.height * centerMarginHeightPercent / 100;
+            // Calculate margin in pixels
+            const horizontalMargin = containerRect.width * marginPercent / 100;
+            const verticalMargin = containerRect.height * marginPercent / 100;
+
+            // // Random position within margins
+            // const x = getRandomPosition(artSetSizes[index].width, horizontalMargin, centerMarginWidth, containerRect.width);
+            // const y = getRandomPosition(artSetSizes[index].height, verticalMargin, centerMarginHeight, containerRect.height);
+
+
+            let x, y, isPositionUnique;
+            do {
+                // Random position within margins
+                x = getRandomPosition(artSetSizes[index].width, horizontalMargin, centerMarginWidth, containerRect.width);
+                y = getRandomPosition(artSetSizes[index].height, verticalMargin, centerMarginHeight, containerRect.height);
+                artSetSizes[index].x = x;
+                artSetSizes[index].y = y;
+
+                isPositionUnique = true;
+                // Check if the position overlaps with any previous positions
+
+                const thresh = 100;
+
+                for (let i = 0; i < artSetSizes.length; i++) {
+                    if(i === index) continue;
+                   
+                    console.log(artSetSizes[i]);
+
+                    if ( Math.abs(artSetSizes[i].x - x) < thresh || Math.abs(artSetSizes[i].y - y) < thresh ) {
+                        
+                        isPositionUnique = false;
+                        break;
+                    }
+                    
+                }
+            } while (!isPositionUnique);
+
+            
+            return {
+                xy: [x, y],
+                config: { mass: slideshow_mass, tension: slideshow_tension, friction: slideshow_friction }
+            };
+        });
+    }, [set, artwork_sets.length, artSetSizes]);
+
+
+
+    // Gesture bindings
+    const bind = useGesture({
+        onDrag: ({ args: [index], movement: [mx, my], memo = springs[index].xy.get() }) => {
+            const [initialX, initialY] = memo;
+            const containerRect = document.querySelector('.gallery-container').getBoundingClientRect();
+
+            // Calculate margin in pixels based on the percentage
+            const horizontalMargin = containerRect.width * marginPercent / 100;
+            const verticalMargin = containerRect.height * marginPercent / 100;
+
+            // Calculate new positions with margin constraints
+            const newX = Math.min(containerRect.width - artSetSizes[index].width - horizontalMargin,
+                Math.max(horizontalMargin, initialX + mx));
+            const newY = Math.min(containerRect.height - artSetSizes[index].height - verticalMargin,
+                Math.max(verticalMargin, initialY + my));
+
+            set(i => {
+                if (index !== i) return;
+                return { xy: [newX, newY] };
+            });
+
+            return memo;
+        }
+    });
+
+    if (!springs.length) return null;
+
+
+    //Function to determine if a slide is the next slide
     const isNextSlide = (artSetIndex, slideIndex) => {
         const nextSlideIndex = (currentSlides[artSetIndex] + 1) % artwork_sets[artSetIndex].items.length;
         return slideIndex === nextSlideIndex;
     };
 
-    // Toggle expanded mode for a specific slideshow
-    const toggleExpanded = (index) => {
-        setExpandedSlideshow(expandedSlideshow === index ? null : index);
-        document.body.classList.toggle('expanded-content', expandedSlideshow === index ? null : index);
-    };
-
     return (
         <div className="gallery-container">
-            {artwork_sets.map((art_set, artSetIndex) => (
-                <div key={artSetIndex} className="art-set">
+            {springs.map(({ xy }, artSetIndex) => (
+                <animated.div
+                    key={artSetIndex}
+                    className="art-set"
+                    style={{ transform: xy.to((x, y) => `translate3d(${x}px,${y}px,0)`) }}
+                    {...bind(artSetIndex)}
+                >
                     <div
-                        className={`slide-container ${expandedSlideshow === artSetIndex ? 'expanded' : ''}`}
-                        onClick={() => toggleExpanded(artSetIndex)}
-                        style={{ 
-                            width: artSetSizes[artSetIndex].width + 'px', 
+                        className={`slide-container`}
+                        style={{
+                            width: artSetSizes[artSetIndex].width + 'px',
                             height: artSetSizes[artSetIndex].height + 'px'
                         }}
                     >
-                        {art_set.items.map((art_item, slideIndex) => (
+                        {artwork_sets[artSetIndex].items.map((art_item, slideIndex) => (
                             <div
                                 key={slideIndex}
                                 className={`slide ${slideIndex === currentSlides[artSetIndex] ? 'current' : ''} ${isNextSlide(artSetIndex, slideIndex) ? 'next' : ''}`}
                             >
                                 <div className="media-hover-container">
                                     <div className="media-container">
-                                        <div className="overlay">
-                                            <h3 className="title">{art_set.title}</h3>
-                                            <p className="description">{art_item.title}: {art_item.description}</p>
-                                        </div>
+                                        {/* <div className="overlay"> */}
+                                            {/* <h4 className="title">{artwork_sets[artSetIndex].title}</h4> */}
+                                            {/* <p className="description">{art_item.title}: {art_item.description}</p> */}
+                                        {/* </div> */}
                                         {art_item.type === 'image' ? (
                                             <img src={art_item.file} alt={art_item.title} className="slide-media" />
                                         ) : art_item.type === 'video' ? (
@@ -87,7 +196,7 @@ export default function Gallery({ artwork_sets }) {
                             </div>
                         ))}
                     </div>
-                </div>
+                </animated.div>
             ))}
         </div>
     );
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,113 @@ import BasicInfo from '../components/BasicInfo';
 import Gallery from '../components/Gallery';
 import fs from "fs";
 import path from 'path';
+import styles from "@/styles/Main.css"
+
+import { useCallback } from "react";
+import Particles from "react-particles";
+import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
+
 
 
 export default function Home({ artwork_sets }) {
+    const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        //await loadFull(engine);
+        await loadSlim(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        console.log(container);
+    }, []);
+
     return (
         <div>
+            <Particles
+                id="tsparticles"
+                init={particlesInit}
+                loaded={particlesLoaded}
+                options={{
+                    // background: {
+                    //     color: {
+                    //         value: "#0d47a1",
+                    //     },
+                    // },
+                    fpsLimit: 240,
+                    interactivity: {
+                        events: {
+                            onClick: {
+                                enable: true,
+                                mode: "push",
+                            },
+                            // onHover: {
+                            //     enable: true,
+                            //     mode: "repulse",
+                            // },
+                            resize: true,
+                        },
+                        modes: {
+                            push: {
+                                quantity: 10,
+                            },
+                            // repulse: {
+                            //     distance: 20,
+                            //     duration: 0.5,
+                            // },
+                        },
+                    },
+                    particles: {
+                        color: {
+                            value: "#000000",
+                        },
+                        links: {
+                            color: "#000000",
+                            distance: 20,
+                            enable: true,
+                            opacity: 0.2,
+                            width: 1,
+                        },
+                        move: {
+                            direction: "none",
+                            enable: true,
+                            outModes: {
+                                default: "bounce",
+                            },
+                            random: false,
+                            speed: 1,
+                            straight: false,
+                        },
+                        number: {
+                            density: {
+                                enable: true,
+                                area: 600,
+                            },
+                            value: 40,
+                        },
+                        opacity: {
+                            value: 0.2,
+                        },
+                        shape: {
+                            type: "circle",
+                        },
+                        size: {
+                            value: { min: 1, max: 3 },
+                        },
+                    },
+                    detectRetina: true,
+                }}
+            />
             <Head>
-                {/* <title>Artist Name - Portfolio</title> */}
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-
-            <main>
-                {/* <h1>Welcome to Artist Name's Portfolio</h1> */}
-                <Gallery artwork_sets={artwork_sets} />
+            <main className='main'>
+                <div className="artist-name">Dante Trisciuzzi</div>
+                <div className='gallery-background'>
+                    <Gallery artwork_sets={artwork_sets} />
+                </div>
             </main>
 
             <footer>
@@ -39,12 +132,12 @@ export async function getStaticProps() {
         try {
             const element = artworkMetadatas[index];
             const metadataPath = path.join(artworksDir, artworkMetadatas[index]);
-            const metaDataRaw = fs.readFileSync(metadataPath, {encoding: 'utf-8'});
+            const metaDataRaw = fs.readFileSync(metadataPath, { encoding: 'utf-8' });
             const metaData = JSON.parse(metaDataRaw);
 
             const assetBasePath = path.join("/artwork", metaData[0]['directory']);
 
-            var art_set = {title: metaData[0]['title'], items: []}
+            var art_set = { title: metaData[0]['title'], items: [] }
 
             metaData[0]['items'].forEach(element => {
                 var art_item = {
